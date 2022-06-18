9e4988731e790feb53bd8e9a09e8717021614068
LuisNavarro24 <83561102+LuisNavarro24@users.noreply.github.com>
2022-06-18 15:03:07 -0500

    Angular correo electrónico
    Actualización correos con angular para formulario de correos

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { AccesibilidadService } from '../shared/accesibilidad.service';
 import { Observable } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'; 
+
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.scss']
 })
 export class ContactoComponent implements OnInit {
-  
-  constructor(public accesibilidad:AccesibilidadService) { }
-
-  ngOnInit(): void {
+  datos:FormGroup;
+  constructor(private httpclien:HttpClient, public accesibilidad:AccesibilidadService){
+    this.datos=new FormGroup({
+      correo:new FormControl('',[Validators.required,Validators.email]),
+      asunto: new FormControl('',Validators.required),
+      msj: new FormControl('',Validators.required)
+    })
   }
 
+  ngOnInit(): void {
+  
+  }
+
+  envio(){
+
+    let params={
+      email:this.datos.value.correo,
+      asunto:this.datos.value.asunto,
+      mensaje:this.datos.value.msj     
+      }
+    console.log(params)
+    
+    this.httpclien.post('http://localhost:3000/envio',params).subscribe(resp=>{
+    console.log(resp)
+    })
+  }
 }
+
+
+
